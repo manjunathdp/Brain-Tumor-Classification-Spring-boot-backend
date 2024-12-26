818f27449fb1ce47ce1e878a3fa33feb0a0ca8d5
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-26 22:43:06 +0530

    initial commit

---
+++ src/main/java/com/braintumor/Brain/Tumor/Detection/and/Classification/BrainTumorDetectionAndClassificationApplication.java
@@ -0,0 +1,13 @@
+package com.braintumor.Brain.Tumor.Detection.and.Classification;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrainTumorDetectionAndClassificationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrainTumorDetectionAndClassificationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/braintumor/Brain/Tumor/Detection/and/Classification/BrainTumorDetectionAndClassificationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.braintumor.Brain.Tumor.Detection.and.Classification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrainTumorDetectionAndClassificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
