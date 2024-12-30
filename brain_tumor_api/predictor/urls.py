412ae9f3e5805a2844c2b0c968b89b224928802a
Manjunath Pujar <manjunathdtc3@gmail.com>
2024-12-30 20:12:36 +0530

    unwanted files deleted

--- brain_tumor_api/predictor/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import PredictTumor
-
-urlpatterns = [
-    path('predict/', PredictTumor, name='predict'),  # Remove .as_view() for function-based views
-]
